e3dd30c97ce2fb7f0824192060bf7b5504e6e7d3
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-12-29 14:53:16 -0300

    finish day

--- meuprojeto/src/components/layout/NavbarIcons.js
+++ meuprojeto/src/components/layout/NavbarIcons.js
@@ -7,20 +7,27 @@ import {
   FaPinterest,
   FaTwitter,
 } from "react-icons/fa";
+import { useState } from "react";
 
 function NavbarIcons({ customClass }) {
-  function showName() {
-    console.log("oi");
-  }
+  const [name, setName] = useState();
+
+  const showName = (e) => {
+    setName(e.target.id);
+    console.log(name);
+  };
+
+  // const hiddenName = (e) => {};
 
   return (
     <nav className={`iconsNav ${customClass}`}>
       <Container customClass="backgroundIcon">
-        <FaFacebook onMouseEnter={showName}></FaFacebook>
-        <FaInstagram></FaInstagram>
-        <FaPinterest></FaPinterest>
-        <FaDiscord></FaDiscord>
-        <FaTwitter></FaTwitter>
+        <FaFacebook onMouseEnter={showName} id="Facebook"></FaFacebook>
+        <FaInstagram onMouseEnter={showName} id="Instragram"></FaInstagram>
+        <FaPinterest onMouseEnter={showName} id="Pinterest"></FaPinterest>
+        <FaDiscord onMouseEnter={showName} id="Discord"></FaDiscord>
+        <FaTwitter onMouseEnter={showName} id="Twitter"></FaTwitter>
+        
       </Container>
       <div className="image"></div>
     </nav>

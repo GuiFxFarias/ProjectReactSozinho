00bc0d6c605c6f887a2a7a2ac97f22f22969a733
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2023-01-05 20:23:51 -0300

    feature: adding stopwatch

--- meuprojeto/src/components/pages/Torneios.js
+++ meuprojeto/src/components/pages/Torneios.js
@@ -1,7 +1,26 @@
-
+import { useEffect, useState } from "react";
 
 function Torneios() {
-  return <p>Torneios</p>;
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [hour, setHour] = useState(0);
+
+  if (seconds >= 60) {
+    setSeconds = 0;
+
+    const intMinutes = setInterval(() => {
+      setMinutes((minutes) => minutes + 1);
+    }, 1000);
+    return () => clearInterval(intMinutes);
+  }
+
+  return (
+    <div className="torneios">
+      <h2>
+        {hour}:{minutes}:{seconds}
+      </h2>
+    </div>
+  );
 }
 
 export default Torneios;
